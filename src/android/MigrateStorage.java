760c39b2f892545b3a0cabeeb637715fa3f2ca14
MGX47Storai <109550078+MGX47Storai@users.noreply.github.com>
2022-11-10 01:51:47 +0200

    test

--- src/android/MigrateStorage.java
+++ src/android/MigrateStorage.java
@@ -27,7 +27,7 @@ import java.io.File;
  */
 public class MigrateStorage extends CordovaPlugin {
     // Switch this value to enable debug mode
-    private static final boolean DEBUG_MODE = false;
+    private static final boolean DEBUG_MODE = true;
 
     private static final String TAG = "com.migrate.android";
     private static final String FILE_PROTOCOL = "file://";
